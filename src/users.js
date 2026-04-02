0ecaa4b4c3d552212f1c9162aaf3f9e4fa668612
NayanShankarGupta <nsgnayan2006@gmail.com>
2026-04-02 12:09:31 +0530

    Update users.js

--- src/users.js
+++ src/users.js
@@ -8,13 +8,6 @@ export let users = [
   },
   {
     id: 2,
-    name: "Anaavil Sharma",
-    phone: "1234567890",
-    password: "anaavil123",
-    role: "patient",
-  },
-  {
-    id: 3,
     name: "Dr. Anaavil Sharma",
     phone: "9876543210",
     password: "anaavil123",
